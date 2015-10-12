21643d7632f960c38644790e309f5adfcf9e10af
Eduard Fuzessery <eduard.fuzessery@yahoo.com>
2015-10-12 17:38:39 +0200

    other histograms

--- LIu1.m
+++ LIu1.m
@@ -3,3 +3,19 @@ values = csvread('B.csv');
 h = figure(1);
 hist(values(:,1),49);
 saveas(h, 'histAges.pdf');
+
+%%
+
+h = figure(1);
+hist(values(:,2),49);
+saveas(h, 'histPressures.pdf');
+
+%%
+h = figure(1);
+hist(values(:,3),19);
+saveas(h, 'histBmi.pdf');
+
+%%
+
+%real analysis begins here
+
