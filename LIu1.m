ca93543a33127583675576c96567f2b799ad141b
Eduard Fuzessery <eduard.fuzessery@yahoo.com>
2015-10-28 11:46:21 +0100

    tex ready

--- LIu1.m
+++ LIu1.m
@@ -13,6 +13,12 @@ hist(data(:,1),70);
 ylabel('Count');
 xlabel('Age');
 
+%%
+
+hist(data(:,1),70);
+ylabel('Count');
+xlabel('Age');
+
 % FirstMedication = data(:,8) >= 1;
 
 % CuredWithFirst = data(:,8);
