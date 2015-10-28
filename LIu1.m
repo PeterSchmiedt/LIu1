32c3e3984e4e119e6a1b5b795965e07d19f89324
Eduard Fuzessery <eduard.fuzessery@yahoo.com>
2015-10-28 13:57:53 +0100

    new Images

--- LIu1.m
+++ LIu1.m
@@ -17,11 +17,24 @@ ylabel('Pocet');
 xlabel('Vek');
 
 %%
-bmi = [18,24,30,40]
-histc(data(:,3),bmi);
-ylabel('Count');
+bmiData= data(:,3);
+bmi = [0,18,19,24,25,29,30,max(bmiData)];
+
+figure(2);
+h = histogram(bmiData);
+h.BinWidth = 5;
+ylim = max(bmiData);
+ylabel('Pocet');
 xlabel('BMI');
 
+%%
+
+pressureData = data(:,2);
+
+figure(3);
+histogram(pressureData);
+ylabel('Pocet');
+xlabel('Priemernı Krvnı Tlak');
 % FirstMedication = data(:,8) >= 1;
 
 % CuredWithFirst = data(:,8);
