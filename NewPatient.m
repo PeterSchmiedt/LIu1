edf98f8ee8280b447c3e8590d4f10803e9854523
Eduard Fűzesséry <fuzesedu@fel.cvut.cz>
2015-11-08 20:58:22 +0100

    ids done

--- NewPatient.m
+++ NewPatient.m
@@ -3,43 +3,43 @@ function ID = NewPatient( age, pressure, bmi, otherMed, data)
 %vek/map/bmi/lek/jina medikace
 
 if (age<30) 
-    ID = '1';
+    ID = 1000;
 elseif (age < 40) 
-    ID = '2';
+    ID = 2000;
 elseif (age<50)
-    ID = '3';
+    ID = 3000;
 elseif (age<60)
-    ID = '4';
-elseif (age>=60)
-    ID = '5';
+    ID = 4000;
+else
+    ID = 5000;
 end
 
 if (pressure < 70) 
-    ID = ID + '1';
+    ID = ID + 100;
 elseif (pressure < 93) 
-    ID = ID + '2';
+    ID = ID + 200;
 elseif (pressure < 106)
-    ID = ID + '3';
+    ID = ID + 300;
 elseif (pressure < 120)
-    ID = ID + '4';
-elseif (pressure >= 120)
-    ID = ID + '5';
+    ID = ID + 400;
+else
+    ID = ID + 500;
 end
 
 if (bmi < 18.5) 
-    ID = ID + '1';
+    ID = ID + 10;
 elseif (bmi < 25) 
-    ID = ID + '2';
+    ID = ID + 20;
 elseif (bmi < 35)
-    ID = ID + '3';
-elseif (bmi >= 35)
-    ID = ID + '4';
+    ID = ID + 30;
+else
+    ID = ID + 40;
 end
 
 if (otherMed == 0) 
-    ID = ID + '0';
-elseif (otherMed == 1)
-    ID = ID + '1';
+    ID = ID + 0;
+else 
+    ID = ID + 1;
 end
 
 end
--- SemestralkaLI.m
+++ SemestralkaLI.m
@@ -1,7 +1,15 @@
 data = csvread('B.csv');
 
-ids = 
+ids = zeros(1, length(data));
 
-for i = length(data)
+%%
+
+for i = 1 : length(ids)
     row = data(i,:);
-    ids[i] = NewPatient(row[1], row[2], row[3], row[7], data);
+    ids(i) = NewPatient(row(1), row(2), row(3), row(7), data);    
+end
+
+%%
+
+h = histogram(ids);
+values = h.Values;
