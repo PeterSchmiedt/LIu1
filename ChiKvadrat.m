8368a10bf8006026dbd3a5b8c1e23ace9d6b45e9
PeterSchmiedt <schmiedt.p@gmail.com>
2015-11-09 23:44:25 +0100

    matlab update

--- ChiKvadrat.m
+++ ChiKvadrat.m
@@ -1,4 +1,20 @@
-data = csvread('B.csv');
-workingData = data;
+data = csvread('output.csv');
 
+for i=1:length(data)
+    
+    expectedF = (data(i,3) + data(i,4)) * 0.2975;
+    expectedS = (data(i,5) + data(i,6)) * 0.8;
+    
+    chisq1 = (data(i,3) - expectedF)^2 / expectedF;
+    chisq2 = (data(i,5) - expectedS)^2 / expectedS;
+    
+    chisq = chisq1 + chisq2;
+    
+    data(i,7) = chisq;
+    
+end
+
+%%
+
+sort(data(:,7));
 
